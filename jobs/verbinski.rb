3e07309ffa348560147c54b025729ad79541844f
Matt Gauger <matt.gauger@gmail.com>
2013-11-03 09:21:42 -0600

    Change verbinski (weather widget) to use US units

--- jobs/verbinski.rb
+++ jobs/verbinski.rb
@@ -13,7 +13,8 @@ forecast_location_lat = ENV['LATITUDE']
 forecast_location_long = ENV['LONGITUDE']
 
 # Unit Format
-forecast_units = "ca" # like "si", except windSpeed is in kph
+#forecast_units = "ca" # like "si", except windSpeed is in kph
+forecast_units = "auto"
 
 def time_to_str(time_obj)
   """ format: 5 pm """
@@ -37,13 +38,15 @@ SCHEDULER.every '5m', :first_in => 0 do |job|
   response = http.request(Net::HTTP::Get.new("/forecast/#{forecast_api_key}/#{forecast_location_lat},#{forecast_location_long}?units=#{forecast_units}"))
   forecast = JSON.parse(response.body)
 
-  currently = forecast["currently"]
+  raise "Verbinski: " + forecast["error"] if forecast.has_key?("error")
+
+  currently = forecast["currently"] || {}
   current = {
-    temperature: currently["temperature"].round,
+    temperature: currently.fetch("temperature", 0).round,
     summary: currently["summary"],
-    humidity: "#{(currently["humidity"] * 100).round}&#37;",
-    wind_speed: currently["windSpeed"].round,
-    wind_bearing: currently["windSpeed"].round == 0 ? 0 : currently["windBearing"],
+    humidity: "#{(currently.fetch("humidity", 0) * 100).round}&#37;",
+    wind_speed: currently.fetch("windSpeed", 0).round,
+    wind_bearing: currently.fetch("windSpeed", 0).round == 0 ? 0 : currently["windBearing"],
     icon: currently["icon"]
   }
 
