3145564512107a83fce46102ca5e518acbc64425
Matt Gauger <matt.gauger@gmail.com>
2013-11-18 18:41:59 -0600

    Begin migrating to Faraday

--- jobs/coderwall.rb
+++ jobs/coderwall.rb
@@ -1,4 +1,4 @@
-require 'net/http'
+require 'faraday'
 require 'json'
 
 require 'dotenv'
@@ -6,15 +6,14 @@ Dotenv.load
 
 SCHEDULER.every '30m', :first_in => '5s' do |job|
   uri = URI.parse("https://coderwall.com/team/#{ENV['CODERWALL_TEAM']}.json")
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  request = Net::HTTP::Get.new(uri.request_uri)
-  response = http.request(request)
+  response = Faraday.get uri
+
   status = JSON.parse(response.body)
 
   name = status['name'].to_s
   rank = status['rank'].to_s
   team_size = status['size'].to_s
+
   moreinfo = name + ", currently at " + team_size.to_s + " members."
 
   send_event("coderwall", { title: "Coderwall Rank", current: rank, moreinfo: moreinfo })
